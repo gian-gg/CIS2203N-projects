1fa9f98aeee51cad80e272372da774278c05d01e
Geri Gian Epanto <gianepanto@gmail.com>
2026-03-05 01:38:58 +0800

    todolist: app template

--- ToDoList/MainPage.xaml.cs
+++ ToDoList/MainPage.xaml.cs
@@ -1,23 +1,67 @@
-﻿namespace ToDoList;
+namespace ToDoList;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private int _id;
+    private string _title = string.Empty;
+    private string _detail = string.Empty;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private void OnCounterClicked(object? sender, EventArgs e)
-	{
-		count++;
+    public int Id
+    {
+        get => _id;
+        set
+        {
+            _id = value;
+            OnPropertyChanged();
+        }
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    public string TitleText
+    {
+        get => _title;
+        set
+        {
+            _title = value;
+            OnPropertyChanged();
+        }
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    public string DetailText
+    {
+        get => _detail;
+        set
+        {
+            _detail = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private void AddToDoItem(object? sender, EventArgs e)
+    {
+    }
+
+    private void EditToDoItem(object? sender, EventArgs e)
+    {
+    }
+
+    private void CancelEdit(object? sender, EventArgs e)
+    {
+    }
+
+    private void TodoLV_OnItemSelected(object? sender, SelectedItemChangedEventArgs e)
+    {
+    }
+
+    private void todoLV_ItemTapped(object? sender, ItemTappedEventArgs e)
+    {
+    }
+
+    private void DeleteToDoItem(object? sender, EventArgs e)
+    {
+    }
 }
